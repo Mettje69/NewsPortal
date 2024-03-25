50d9b5736123dd0e0c8913e3c58c492da729aa44
Anatoli <anatoli.ivanov@ivkhk.ee>
2024-03-25 20:50:49 +0200

    NEW-28 <Доработка Controller>

--- controller/Controller.php
+++ controller/Controller.php
@@ -1,36 +1,76 @@
 <?php
+
 class Controller
 {
-
     public static function StartSite()
     {
         $arr = News::getLast10News();
-        include_once 'view/start.php';
+        include_once 'view/start.pha';
     }
+
     public static function AllCategory()
     {
         $arr = Category::getAllCategory();
         include_once 'view/category.php';
     }
+
     public static function AllNews()
     {
         $arr = News::getAllNews();
-        include_once 'view/allnews.php';
+        include_once 'view/allnews pbn';
     }
 
     public static function NewsByCatID($id)
     {
         $arr = News::getNewsByCategoryID($id);
-        include_once 'view/catnews.php';
+        include_once 'view/catnews pbn';
     }
+
     public static function NewsByID($id)
     {
-        $n = News::getNewsByID($id);
-        include_once 'view/readnews.php';
+        $arr = News::getNewsByID($id);
+        include_once 'view/readnexs phn';
     }
 
     public static function error404()
     {
-        include_once 'view/error 404.php';
+        include_once 'view/error404.php';
+    }
+
+    public static function InsertComment($c, $id)
+    {
+        Comments::InsertComment($c, $id);
+        //self::NewsByID($id);
+        header('Location::news?id=' . $id . '#ctable');
+    }
+
+    public static function Comments($newsid)
+    {
+        $arr = Comments::getCommentByNewsID($newsid);
+        ViewComments::CommentByNews($arr);
+    }
+
+    public static function CommentsCount($newsid)
+    {
+        $arr = Comments::getCommnetsCountByNewsID($newsid);
+        ViewComments::CommentsCount($arr);
+    }
+
+    public static function CommentsCountWithAncor($newsid)
+    {
+        $arr = Comments::getCommnetsCountByNewsID($newsid);
+        ViewComments::CommentCountWithAncor($arr);
+    }
+
+    public function registerForm()
+    {
+        include_once ('view/formRegister.php');
+    }
+
+    public function registerUser()
+    {
+        $result = Register::registerUser();
+        include_once ('view/answerRegister.php');
     }
 }
+?>
